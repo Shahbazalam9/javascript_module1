d15cab0e4dcd9c0d1f88facee7d7ac11dc0f9c86
Shahbazalam9 <108791997+Shahbazalam9@users.noreply.github.com>
2023-11-27 17:22:14 +0000

    typeConversuion done

---
+++ 01_basics/typeConversion.js
@@ -0,0 +1,49 @@
+let score = 33;
+// console.log(typeof score) // Number 
+
+let intToString = String(score)
+// console.log(intToString)
+// console.log(typeof intToString) // String
+
+let value = ""
+// console.log(typeof value) // String
+// let toBool =  Boolean(value) false
+// console.log(toBool)
+// if we convert empty string to Boolean then it is false
+
+let value2 = "Shabaz";
+// console.log(typeof value2) // String
+// let toBool =  Boolean(value2) // true
+// console.log(toBool)
+// if we convert  string to Boolean then it is true
+
+
+let value3 = "33abc"
+
+// let stringToInt = Number(value3); // NaN (not a number)
+// console.log(stringToInt)
+// console.log(value3)
+
+let value4 = null;
+// let nullToInt = Number(value3); // NaN (not a number)
+// console.log(value4)
+// console.log(nullToInt)
+
+let value5 = undefined;
+// let undefinedToInt = Number(value3); // NaN (not a number)
+// console.log(value5)
+// console.log(undefinedToInt)
+
+
+// ****************** Operator *******************
+
+
+// console.log(1 + 2 + "2")// 32 => first add then concate
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + "2" + 2) // 122
+
+
+// console.log(+true) // 1
+// console.log(true+) // error
+
+
