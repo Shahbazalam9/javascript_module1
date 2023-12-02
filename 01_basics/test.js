473db7de3605dcab2388cb96aea6ac6f5f2a0be9
Shahbazalam9 <108791997+Shahbazalam9@users.noreply.github.com>
2023-12-02 17:57:16 +0000

    change in test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Shahbaz")
+const myNums = [1,2,3,4,5,6,7,8,9]
+
+const newval = myNums.reduce( (acc, curr) =>{
+    return acc + curr
+},0)
+// console.log(newval)
+
+
+const val = myNums.reduce( (acc,curr)=> acc+curr,0)
+console.log(val)
